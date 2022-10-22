0e1f9dd1c7a444729003c9e0bad5df5d2d13e4dc
Olive Davis <olivedavis@my.unt.edu>
2022-10-22 13:47:02 -0500

    industrial cpp updated

--- cppfolder/industrialzone.cpp
+++ cppfolder/industrialzone.cpp
@@ -5,3 +5,41 @@
 industrialzone::industrialzone() {
 
 }
+
+int industrialzone::GetPopulation(){
+    return population;
+}
+	
+void SetPopulation(int population){
+    this->population = population;
+}
+	
+int GetAvailableGoods(){
+        return availableGoods;
+    }
+	
+void SetAvailableGoods(int availableGoods){
+    this->availableGoods;
+}
+	
+int GetWorkerCount(){
+    return workerCount;
+}
+	
+void SetWorkerCount(int workerCount){
+    this->workerCount;
+}
+	
+
+//Grow zone population
+int IncreasePopulation(bool power, int workerCount, int population){ 
+    if(power == true && workerCount >= 2){
+        population = population + 1;
+
+    }
+
+    else {
+        return population;
+    }
+
+}
