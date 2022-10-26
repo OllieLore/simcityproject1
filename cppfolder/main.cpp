ba1e227023c154873a437556a27f12aa0dadd11d
Marc Nikirk <MarcNikirk@my.unt.edu>
2022-10-26 15:41:55 -0500

    changed to 2d vector (node class not in use)

--- cppfolder/main.cpp
+++ cppfolder/main.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 #include <vector>
-#include "zonenode.cpp" // ***Reminder: change .cpp to .h before uplaod
+#include "zonenode.h" // ***Reminder: change .cpp to .h before uplaod
 //#include "comercialzone.h"
 //#include "residential.h"
 //#include "industrial.h"
@@ -16,9 +16,11 @@ int main()
     string valueRead, inputFile; // variable for node character (stored as string to use getline function)
     vector<zonenode*> listHeads; // stores head of the linked list {setup for Linked List implimentation}
     zonenode* tempNodePtr = NULL;
-    bool headNode;
+    bool headNode = false;
     int numberOfNodes = 0;
     int intRead;
+
+    vector<vector<char>> regionMap;
 
     ifstream inputStream; // file reader input 
 
@@ -35,110 +37,60 @@ int main()
     */
 
     //-------------------------- Reading in Region file --------------------------
-    inputStream.open(/*inputFile*/ "CityData.txt" );
+    inputStream.open(/*inputFile*/ "CityCSV.txt");
     if (inputStream.is_open()) // saftey check for open file
     {
 
         while (!inputStream.eof()) // start reading file until the end
         {
             getline(inputStream, valueRead, ','); // read until next comma and store it in valueRead
-            zonenode* newNode = new zonenode(); // Make empty node
+            
 
-            while (valueRead.at(0) == '\n' && !inputStream.eof())
+            if (regionMap.empty()) // checks if there is no rows
             {
-                
-                valueRead = valueRead.at(1); // removes the newline characeter
-                headNode = true; // set marker so that later can be turned into head of new linked list
-                
+                headNode = true; // mark for new row vector
             }
 
-            switch (valueRead.at(0)) // use input to add the correct node
+            for (int i = 0; i < valueRead.size(); i++) // checking for new lines
             {
-            case 'R':
-                /* code */
-                break;
-            
-            case 'I':
-                /* code */
-                break;
-            
-            case 'C':
-                /* code */
-                break;
-            
-            case 'T':
-                /* code */
-                break;
-            case '#':
-                /* code */
-                break;
-            
-            case '-':
-                /* code */
-                break;
-            
-            case 'P':
-                /* code */
-                break;
-            
-            default:
-                break;
-            }
-            
-            if (listHeads.empty())
-            {
-                headNode = true;
+                if (valueRead.at(i) == '\n')
+                {
+                    if (i == 0)
+                    {
+                        headNode = true; // set marker so that later can be turned into head of new linked list
+                        valueRead = valueRead.at(1); // removes the newline characeter
+                    }
+                    else
+                    {
+                        valueRead = valueRead.at(0); // removes the newline characeter
+                    }
+                }
             }
 
-            newNode->setType(valueRead.at(0)); // sets value read to be stored into the empty node
-            numberOfNodes++; // increase node count
-
-            newNode->setNeighbor(2, tempNodePtr); // sets previous node pointer
-
-            if (tempNodePtr != NULL) 
+            if (headNode) // adds new row vector
             {
-                tempNodePtr->setNeighbor(3, newNode); //sets next node value
-            }
-            
-
-            tempNodePtr = newNode;
-
-            if (headNode) // adds marked nodes to head vector
-            {
-                listHeads.push_back(newNode);
+                vector<char> tempCarVect;
+                regionMap.push_back(tempCarVect);
                 headNode = false;
             }
-        }
-        tempNodePtr->setNeighbor(3, NULL); // sets tails node to NULL
-
-        for (long unsigned int i = 1; i < listHeads.size(); i++) // cuts the list into seperate list 
-        {
-            tempNodePtr = listHeads.at(i)->getNeighbor(2);
-            tempNodePtr->setNeighbor(3, NULL);
-            listHeads.at(i)->setNeighbor(2, NULL);
+            
+            regionMap.at(regionMap.size() - 1).push_back(valueRead.at(0)); // adds valueRead to its row vector
         }
 
-
-        zonenode* curNode;
-        for (long unsigned int i = 0; i < listHeads.size(); i++) // keeps track of row
+        for (long unsigned int i = 0; i < regionMap.size(); i++) // outputs regionMap 
         {
-            curNode = listHeads.at(i);
-            for (long unsigned int j = 0; curNode != NULL; j++) // keeps track of collumn
+            if (regionMap.at(i).size() != 1 && regionMap.at(i).at(0) != ' ')
+            for (long unsigned int j = 0; j < regionMap.at(i).size(); j++)
             {
-                if (j > 0)
-                {
-                    std::cout << curNode->getType() << ", ";
-                    curNode = curNode->getNeighbor(3);
-                }
-                
+                cout << regionMap.at(i).at(j) << ",";
             }
-            std::cout << endl;
+            cout << endl;
         }
 
-        std::cout << "DONE" << endl; //DEBUG: make sure script is fully run
+        cout << "DONE" << endl; //DEBUG: make sure script is fully run
 
         inputStream.close(); // close file
     }
-    
+
     return 0;
 }
