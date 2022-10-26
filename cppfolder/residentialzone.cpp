743d6330534163b4d1b228b23ffc3327325c6975
Haynes, Elijah <elijahhaynes@my.unt.edu>
2022-10-26 01:05:14 +0000

    Update residentialzone.cpp

--- cppfolder/residentialzone.cpp
+++ cppfolder/residentialzone.cpp
@@ -4,9 +4,7 @@
 //Constructor
 residentialzone::residentialzone()
 {
-    int population; // Keeps track of the population in a cell
-    int Neighbors; // Keeps track of population size of the cell's neighbors
-    bool IsNearPowerline, PopulationisGreater; // Determines if the conditions are met to increase the population size
+
 };
 
 int residentialzone::getPopulationSize(int population)
