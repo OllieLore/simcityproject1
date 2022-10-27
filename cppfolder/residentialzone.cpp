d6d9e18fc92e1cd3774e5c851a0b35060d4af1b3
Haynes, Elijah <elijahhaynes@my.unt.edu>
2022-10-27 15:14:38 +0000

    residentialzone.cpp should be finished

--- cppfolder/residentialzone.cpp
+++ cppfolder/residentialzone.cpp
@@ -4,25 +4,79 @@
 //Constructor
 residentialzone::residentialzone()
 {
-    int population = 0; // Population starts at 0
-    int Neighbors = 0; // Neighbors starts at 0
-    bool IsNearPowerline = false; // Set to false by default
-    bool PopulationisGreater = false; // Set to false by default
+    population = 0; // Population starts at 0
+    Neighbors = 0; // Neighbors starts at 0
+    PopulationisGreater = false; // Set to false by default
 };
 
 int residentialzone::getPopulationSize(int population)
 {
     return population;
-    // WILL NEED TO UTILIZE RECURSION TO FIND OUT IF THE POPULATION ZONE IS NEAR A POWERLINE AND NEAR A NODE WITH A HIGHER POPULATION
-    // KEEP POPULATION AT LIMIT OF 5, UNLESS TA SAYS OTHERWISE
 };
 
-int residentialzone::IncreasePopulationSize(int population, int Neighbors, bool IsNearPowerline, bool PopulationisGreater)
+int residentialzone::IncreasePopulationSize(int population, int Neighbors)
 {
-    if(IsNearPowerline == true && PopulationisGreater == true)
+    vector<int> tempPop; // Looks at the neighbors of the cell
+    IsNearPowerline = false; // Set to false by default
+
+    for(int x = 0; x < 8; x++) // Initializes and defines its neighbors
     {
-        // TRY AND KEEP POPULATION LIMITED TO 5
-        // POTENTIALLY HAVE RECURSION IN A LOOP THAT STOPS WHEN POP REACHES 5
-        population+1;
+        zonenode *temp = getNeighbor(x);
+
+        switch (temp->getType())
+        {
+            case 'T':
+                IsNearPowerline = true;
+                break;
+            case '#':
+                IsNearPowerline = true;
+                break;
+            case 'I':
+                tempPop.push_back(temp->getPopulation());
+                break;
+            case 'C':
+                tempPop.push_back(temp->getPopulation());
+                break;
+            case 'R':
+                tempPop.push_back(temp->getPopulation());
+                break;
+        }
     }
+    if (IsNearPowerline) // If the cell is confirmed to be near a powerline
+    {
+        population+=1; // Increases population by one
+    }
+        switch (population) // Checks neighboring cells for if the population is greater
+        {
+        case 0:
+            if(count(tempPop.begin(), tempPop.end(), 1) >= 1)
+            {
+                population++;
+            }
+            break;
+        case 1:
+            if(count(tempPop.begin(), tempPop.end(), 1) >= 2)
+            {
+                population++;
+            }
+            break;
+        case 2:
+            if(count(tempPop.begin(), tempPop.end(), 2) >= 4)
+            {
+                population++;
+            }
+            break;
+        case 3:
+            if(count(tempPop.begin(), tempPop.end(), 3) >= 6)
+            {
+                population++;
+            }
+            break;
+        case 4:
+            if(count(tempPop.begin(), tempPop.end(), 4) >= 8)
+            {
+                population++;
+            }
+            break;
+        }
 };
