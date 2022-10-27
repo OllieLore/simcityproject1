d96567e9ef59082be9e65912a6294ed216a9c4c7
Olive Davis <olivedavis@my.unt.edu>
2022-10-26 19:08:44 -0500

    got x coordinate working

--- project1/readincsv.cpp
+++ project1/readincsv.cpp
@@ -5,6 +5,7 @@
 #include <sstream>
 #include "zonenode.h"
 #include <cstring>
+#include <algorithm>
 
 using namespace std;
 
@@ -31,64 +32,74 @@ int main(){
       return 1;
    }
 
-   while (!csvReadIn.fail()) {
+
+  while (!csvReadIn.fail()) {
 
     getline(csvReadIn, line, ',');
 
     initialZone.push_back(line);
-    getline(csvReadIn, row, '\n');
-         rowCount.push_back(row);
-       
-        cout << row.size();
-      
-        }
 
-rows = rowCount.size();
+    getline(csvReadIn, row, '\n');
+
+         rowCount.push_back(row);
+     for (int i = 0; i < row.size(); i++){
+    if (row[i] == ',') { 
+        
+        count++;
+  rows = rowCount.size();
+  zoneNode.SetXCoordinate(count);
+  zoneNode.SetYCoordinate(rows);
+  mapNodes.push_back(zoneNode);
+
+    }
+    
+}
+    }
+        
+
+
 
 for (i = 0; i < initialZone.size(); ++i) {
  
-zoneNode.SetXCoordinate(i);
-zoneNode.SetYCoordinate(i);
+
 temp = initialZone.at(i);
 
  if(temp == "-"){
-zoneNode.setType('-');
+mapNodes.at(i).setType('-');
 }
  
 if(temp == "P"){
-zoneNode.setType('P');
+mapNodes.at(i).setType('P');
 }
 
 if(temp == "T"){
-zoneNode.setType('T');
+mapNodes.at(i).setType('T');
 }
 
 if(temp == "I"){
-zoneNode.setType('I');
+mapNodes.at(i).setType('I');
 }
 
 if(temp == "R"){
-zoneNode.setType('R');
+mapNodes.at(i).setType('R');
 }
 if(temp == "C"){
-zoneNode.setType('C');
+mapNodes.at(i).setType('C');
 }
 
 if(temp == "#"){
-zoneNode.setType('#');
+mapNodes.at(i).setType('#');
 }
 
-mapNodes.push_back(zoneNode);
-
 }
 
-cout<< mapNodes.at(6).GetXCoordinate() << mapNodes.at(6).GetYCoordinate() << endl;
-cout<< mapNodes.at(6).getType() << endl;
+for(i=0; i<mapNodes.size(); ++i){
+cout<< mapNodes.at(i).GetXCoordinate() << mapNodes.at(i).GetYCoordinate() << endl;
+cout<< mapNodes.at(i).getType() << endl;
+}
 
 csvReadIn.close();
 
-cout << rows << endl;
-//cout << mapNodes.at(6) << endl;
 
 return 0;
 
