d296fc03ea69bfe7feb02c4f0e86d131b2c2847e
Marc Nikirk <MarcNikirk@my.unt.edu>
2022-10-26 19:08:19 -0700

    finishing touches for milesone 1. Set up for config file and vector is now working off of the zone node class.

--- cppfolder/main.cpp
+++ cppfolder/main.cpp
@@ -17,77 +17,92 @@ int main()
     bool headNode = false;
     int intRead;
 
-    vector<vector<char>> regionMap;
+    vector<vector<zonenode>> regionMap;
 
-    ifstream inputStream; // file reader input 
+    ifstream inputStream, in_s; // file reader input 
 
 
     //-------------------------- Reading in Config file --------------------------
-    /*
-    cin >> inputFile; // Read in the region file's name
+    
+    cout << "Input config file:";
+    cin >> inputFile; // Read in the config file's name
 
-    cin >> intRead; // Read in the max time steps
-    const int MAX_TIME_STEPS  = intRead; // Store max time steps
-
-    cin >> intRead; // Read in the refresh rate of time steps
-    const int REFRESH_RATE  = intRead; //  Store refresh rate of time steps
-    */
-
-    //-------------------------- Reading in Region file --------------------------
-    inputStream.open(/*inputFile*/ "CityCSV.txt");
-    if (inputStream.is_open()) // saftey check for open file
+    in_s.open(inputFile);
+    if (in_s.is_open())
     {
+        in_s >> inputFile; // Read in the region map file's name
 
-        while (!inputStream.eof()) // start reading file until the end
+        in_s >> intRead; // Read in the max time steps
+        const int MAX_TIME_STEPS  = intRead; // Store max time steps
+        cout << "Max Time Steps: " << MAX_TIME_STEPS << endl;
+
+        in_s >> intRead; // Read in the refresh rate of time steps
+        const int REFRESH_RATE  = intRead; //  Store refresh rate of time steps
+        cout << "Refresh Rate: " << REFRESH_RATE << endl;
+
+        //-------------------------- Reading in Region file --------------------------
+        inputStream.open(inputFile);
+        if (inputStream.is_open()) // saftey check for open file
         {
-            getline(inputStream, valueRead, ','); // read until next comma and store it in valueRead
-            
 
-            if (regionMap.empty()) // checks if there is no rows
+            while (getline(inputStream, valueRead, ',')) // start reading file until the end
             {
-                headNode = true; // mark for new row vector
-            }
 
-            for (int i = 0; i < valueRead.size(); i++) // checking for new lines
-            {
-                if (valueRead.at(i) == '\n')
+                if (regionMap.empty()) // checks if there is no rows
                 {
-                    if (i == 0)
+                    headNode = true; // mark for new row vector
+                }
+
+                for (int i = 0; i < valueRead.size(); i++) // checking for new lines
+                {
+                    if (valueRead.at(i) == '\n')
                     {
-                        headNode = true; // set marker so that later can be turned into head of new linked list
-                        valueRead = valueRead.at(1); // removes the newline characeter
-                    }
-                    else
-                    {
-                        valueRead = valueRead.at(0); // removes the newline characeter
+                        if (i == 0)
+                        {
+                            headNode = true; // set marker so that later can be turned into head of new linked list
+                            valueRead = valueRead.at(1); // removes the newline characeter
+                        }
+                        else
+                        {
+                            valueRead = valueRead.at(0); // removes the newline characeter
+                        }
                     }
                 }
-            }
 
-            if (headNode) // adds new row vector
+                
+
+                if (headNode) // adds new row vector
+                {
+                    vector<zonenode> tempZoneVect;
+                    regionMap.push_back(tempZoneVect);
+                    headNode = false;
+                }
+
+                zonenode tempNode;
+                tempNode.setType(valueRead.at(0));
+                regionMap.at(regionMap.size() - 1).push_back(tempNode); // adds valueRead to its row vector
+            }
+                
+            for (long unsigned int i = 0; i < regionMap.size(); i++) // outputs regionMap 
             {
-                vector<char> tempCarVect;
-                regionMap.push_back(tempCarVect);
-                headNode = false;
+                if (regionMap.at(i).size() != 1 && regionMap.at(i).at(0).getType() != ' ')
+                for (long unsigned int j = 0; j < regionMap.at(i).size(); j++)
+                {
+                    cout << regionMap.at(i).at(j).getType() << ",";
+                }
+                cout << endl;
             }
-            
-            regionMap.at(regionMap.size() - 1).push_back(valueRead.at(0)); // adds valueRead to its row vector
-        }
 
-        for (long unsigned int i = 0; i < regionMap.size(); i++) // outputs regionMap 
+            inputStream.close(); // close file
+        }
+        else
         {
-            if (regionMap.at(i).size() != 1 && regionMap.at(i).at(0) != ' ')
-            for (long unsigned int j = 0; j < regionMap.at(i).size(); j++)
-            {
-                cout << regionMap.at(i).at(j) << ",";
-            }
-            cout << endl;
+            cout << "NO" << endl;
         }
 
-        cout << "DONE" << endl; //DEBUG: make sure script is fully run
-
-        inputStream.close(); // close file
+        in_s.close();
     }
+    
 
     return 0;
 }
