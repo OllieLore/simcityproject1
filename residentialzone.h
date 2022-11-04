cd361b34b5b901f58fcf3a4f4fdc2cdc15d49fac
Haynes, Elijah <elijahhaynes@my.unt.edu>
2022-11-04 17:36:28 +0000

    Updated residentialzone.h Removed redundant lines due to inheritance

--- residentialzone.h
+++ residentialzone.h
@@ -6,16 +6,27 @@
 class residentialzone : public zonenode { // Calls from the zonenode from the zonenode.h file
     private:
         static int workers; // Keeps track of the worker amount
-        int population; // Keeps track of the population in a cell
+
+        //int population; // Keeps track of the population in a cell
+                            //REDUNDANT DUE TO INHERITANCE
         int Neighbors; // Keeps track of population size of the cell's neighbors
+                            //REDUNDANT DUE TO INHERITANCE
+
         bool IsNearPowerline, PopulationisGreater; // Determines if the conditions are met to increase the population size
+        int futurePop;
     public:
         residentialzone(); // Class called
         void IncreasePopulationSize(int population, int Neighbors); // Brings in the bools and int to increase the population size
-        int getPopulationSize(int population); // Gets the size of the population
+
+        //int getPopulationSize(int population); // Gets the size of the population
+
         static int getWorkers();
         static void setWorkers(int workers);
-        void growWorkerCount();
+
+        //void growWorkerCount();
+
+        void SetFuturePop(int futurePopI);
+        void UpdatePopAndWorkers();
 };
 
 #endif
