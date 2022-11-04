4ed904e6c11578b1ebefbded237e1be9b466ce05
Haynes, Elijah <elijahhaynes@my.unt.edu>
2022-11-04 17:42:07 +0000

    Updated commercialzone.cpp, should now be up to date

--- commercialzone.cpp
+++ commercialzone.cpp
@@ -2,16 +2,38 @@
 #include "zonenode.h"
 
 //Constructor
-commercialzone::commercialzone() {}
-
-bool commercialzone::GetIsPowered()
+commercialzone::commercialzone() : zonenode::zonenode(nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, 'C', 0) 
 {
-    return isPowered;
+    futurePop = 0;
 }
 
-void commercialzone::SetIsPowered(bool powerI)
+int commercialzone::GetFuturePop()
 {
-    isPowered = powerI;
+    return futurePop;
+}
+
+void commercialzone::SetFuturePop(int futurePopI)
+{
+    futurePop = futurePopI;
+}
+
+bool commercialzone::NeighborPopulationCheck(int popMin, int neighborAmount)
+{
+    int qualified = 0;
+    for (int i = 0; i < 8; i++)
+    {
+        if (getNeighbor(i) != nullptr)
+        {
+            if (getNeighbor(i)->getPopulation() >= popMin)
+            {
+                qualified++;
+            }
+        }
+    }
+    if (qualified >= neighborAmount)
+        return true;
+    else
+        return false;
 }
 
 // might switch to a zonenode function
@@ -26,43 +48,37 @@ void commercialzone::CheckForPower()
     }
 }
 
-bool commercialzone::NeighborPopulationCheck(int popMin, int neighborAmount)
-{
-    int qualified = 0;
-    for (int i = 0; i < 8; i++)
-    {
-        if (getNeighbor(i).getPopulation <= popMin)
-        {
-            qualified++;
-        }
-    }
-
-    if (qualified >= neighborAmount)
-        return true;
-    else
-        return false;
-}
-
-commercialzone::ComercialTimeStep()
+void commercialzone::ComercialTimeStep(int &availableWorker, int availableGood)
 {
     switch(population)
     {
     case 0:
-        if (isPowered /*&& availableWorker && availableGood*/)
+        if (isPowered && availableWorker >= 1 && availableGood >= 1)
         {
-            population = 1;
+            futurePop = 1;
+            availableWorker--;
+            availableGood--;
         }
-        else if (NeighborPopulationCheck(1, 1)/*&& availableWorker && availableGood*/)
+        else if (NeighborPopulationCheck(1, 1) && availableWorker >= 1 && availableGood >= 1)
         {
-            population = 1;
+            futurePop = 1;
+            availableWorker--;
+            availableGood--;
         }
         break;
 
     case 1:
-        if (NeighborPopulationCheck(1, 2)/*&& availableWorker && availableGood*/)
+        if (NeighborPopulationCheck(1, 2) && availableWorker >= 1 && availableGood >= 1)
         {
-            population = 2;
+            futurePop = 2;
+            availableWorker--;
+            availableGood--;
         }
         break;
     }
+}
+
+void commercialzone::UpdatePop()
+{
+    population - GetFuturePop();
 }
