b656d678674b406d5656d6157bbbfda427ecab1d
Haynes, Elijah <elijahhaynes@my.unt.edu>
2022-11-04 03:09:46 +0000

    Updated residentialzone.cpp added workers

--- residentialzone.cpp
+++ residentialzone.cpp
@@ -4,9 +4,10 @@
 #include <algorithm>
 
 //Constructor
+int residentialzone::workers = 0;
 residentialzone::residentialzone() : zonenode::zonenode(nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr, 'R', 0)
 {
-    
+    workers = 0; // Worker amount starts at 0
     population = 0; // Population starts at 0
     Neighbors = 0; // Neighbors starts at 0
     PopulationisGreater = false; // Set to false by default
@@ -16,6 +17,14 @@ int residentialzone::getPopulationSize(int population)
 {
     return population;
 };
+
+int residentialzone::getWorkers() {
+    return workers;
+}
+
+void residentialzone::setWorkers(int workers) {
+    workers = workers;
+}
 
 void residentialzone::IncreasePopulationSize(int population, int Neighbors)
 {
@@ -48,6 +57,7 @@ void residentialzone::IncreasePopulationSize(int population, int Neighbors)
     if (IsNearPowerline) // If the cell is confirmed to be near a powerline
     {
         population+=1; // Increases population by one
+        workers++;
     }
         switch (population) // Checks neighboring cells for if the population is greater
         {
@@ -55,30 +65,35 @@ void residentialzone::IncreasePopulationSize(int population, int Neighbors)
             if(std::count(tempPop.begin(), tempPop.end(), 1) >= 1)
             {
                 population++;
+                workers++;
             }
             break;
         case 1:
             if(count(tempPop.begin(), tempPop.end(), 1) >= 2)
             {
                 population++;
+                workers++;
             }
             break;
         case 2:
             if(count(tempPop.begin(), tempPop.end(), 2) >= 4)
             {
                 population++;
+                workers++;
             }
             break;
         case 3:
             if(count(tempPop.begin(), tempPop.end(), 3) >= 6)
             {
                 population++;
+                workers++;
             }
             break;
         case 4:
             if(count(tempPop.begin(), tempPop.end(), 4) >= 8)
             {
                 population++;
+                workers++;
             }
             break;
         }
